898fea110a1a4090640e5b96b6dbbe92af23891f
Artem Kulin <kulinartem@gmail.com>
2021-04-01 19:51:44 +0300

    modify scripts.js

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,34 +1,78 @@
 'use strict';
 
-fetch("/api/users").then(
-    res => {
-        res.json().then(
-            data => {
-                if (data.length > 0) {
-                    let temp = "";
+async function getData (url) {
+    let res = await fetch(url);
 
-                    data.forEach((elem) => {
-                        temp += "<tr>";
-                        console.log(temp);
-                        temp += "<td>" + elem.id + "</td>";
-                        console.log(temp);
-                        temp += "<td>" + elem.name + "</td>";
-                        console.log(temp);
-                        temp += "<td>" + elem.lastName + "</td>";
-                        console.log(temp);
-                        temp += "<td>" + elem.age + "</td>";
-                        console.log(temp);
-                        // forEach((el) => {console.log(el.role)});
-                        console.log(temp);
-                        temp += "<td>";
-                        console.log(temp);
-                        elem.authorities.forEach((t) => {temp += t.role + " ";})
-                        temp += "</td>";
-                        console.log(temp);
-                    })
-                    document.getElementById("data").innerHTML = temp;
-                }
-            }
-        )
+    if(!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`)
     }
-);
+    return await res.json();
+}
+
+getData("/api/users")
+    .then(data => {
+
+        let tbody = "";
+
+        if(data.length >0) {
+
+            data.forEach((elem) => {
+                tbody += "<tr>";
+                tbody += "<td>" + elem.id + "</td>";
+                tbody += "<td>" + elem.name + "</td>";
+                tbody += "<td>" + elem.lastName + "</td>";
+                tbody += "<td>" + elem.age + "</td>";
+                tbody += "<td>" + elem.email + "</td>";
+                tbody += "<td>"
+                elem.authorities.reverse();
+                elem.authorities
+                    .forEach(
+                        (t) => {
+                            tbody += t.role.substr(5) + " ";
+                        })
+                tbody += "</td>";
+                tbody += "<td>" +
+                    "<a type='button' class='btn btn-info editButton' " +
+                    "data-toggle='modal' " +
+                    "th:href='@{/api/edit/" + elem.id + "}'" + ">Edit</a>" +
+                    "</td>";
+                tbody += "<td>" +
+                    "<a type='button' class='btn btn-danger deleteButton' " +
+                    "data-toggle='modal' " +
+                    "th:href='@{/api/delete/" + elem.id + "}'" + ">Edit</a>" +
+                    "</td>";
+            });
+            document.getElementById("tableContent").innerHTML = tbody;
+        }
+    });
+
+getData("api/security").then(data => {
+    let navItem = "<li class='nav-item' style='color: white'>";
+    navItem += "<strong>" + data.email + "</strong>";
+    navItem += " with roles ";
+
+    let role = null;
+    data.authorities.reverse();
+    data.authorities
+        .forEach((a) => {
+            navItem += a.role.substr(5) + " ";
+            if (a.role.includes("ROLE_ADMIN")) {
+                role = "admin";
+            }
+        });
+    navItem += "<li>";
+    document.getElementById("currentUser").innerHTML = navItem;
+
+    let tab = document.getElementById('v-pills-profile-tab');
+    let div = document.getElementById('v-pills-profile');
+
+    if (role == null) {
+        tab.classList.add('active');
+        div.classList.add('show');
+        div.classList.add('active');
+    }
+
+});
+
+
+
